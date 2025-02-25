7d750f4e380ed061510d3dd2364de3448e12e24f
mohammadsalamin <msalameen.123456@gmail.com>
2025-02-26 00:08:12 +0200

    create user model

---
+++ DB/models/user.model.js
@@ -0,0 +1,51 @@
+import { required } from "joi";
+import mongoose, { model, Schema } from "mongoose";
+
+const userSchema = new Schema({
+    userName : {
+        type : String,
+        required : true,
+        min : 3,
+        max : 20
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    password : {
+        type : String,
+        required : true,
+        min : 4
+    },
+    image : {
+        type : Object
+    },
+    phone : {
+        type : String
+    },
+    address : {
+        type : String
+    },
+    confirmEmail : {
+        type : Boolean,
+        default : false
+    },
+    gender : {
+        type : String,
+        enum : ["Male", "Female"]
+    },
+    status : {
+        type : String,
+        enum : ["Active", "Not_Active"]
+    },
+    role : {
+        type : String,
+        enum : ["Admin", "User"]
+    }
+}, {
+    timestamps : true
+});
+
+const userModel = mongoose.models.User || model('User', userSchema);
+export default userModel;
