1a333f720c991a9332a3f87ec97a7267f92704af
mohammadsalamin <msalameen.123456@gmail.com>
2025-03-06 23:16:47 +0200

    confirm email feature (completed)

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,6 +1,7 @@
 import UserModel from "../../../DB/models/user.model.js";
 import bcrypt from "bcryptjs";
 import sendEmail from "../../utils/sendEmail.js";
+import jwt from 'jsonwebtoken'
 
 export const register = async(req, res, next) =>{
     const {userName, email, password} = req.body;
@@ -10,10 +11,11 @@ export const register = async(req, res, next) =>{
     }
     const hashedPassword = await bcrypt.hash(password, Number(process.env.SALT_ROUND));
     const createdUser = await UserModel.create({userName, email, password : hashedPassword});
+    const token = jwt.sign({email}, process.env.SIGNATURE);
     const html = `
     <div>
         <h1>Welcome ${userName}</h1>
-        <a href = 'http://localhost:3000/auth/confirmEmail/${email}'> Confirm Your Email</a>
+        <a href = '${req.protocol}://${req.headers.host}/auth/confirmEmail/${token}'> Confirm Your Email</a>
     </div>`;
 
     await sendEmail(email, 'Confirm Email', html);
@@ -21,7 +23,8 @@ export const register = async(req, res, next) =>{
 }
 
 export const confirmEmail = async (req, res) =>{
-    const {email} = req.params;
-    await UserModel.findOneAndUpdate({email : email}, {confirmEmail : true});
+    const {token} = req.params;
+    const decoded = jwt.verify(token, process.env.SIGNATURE);
+    await UserModel.findOneAndUpdate({email : decoded.email}, {confirmEmail : true});
     return res.status(200).json({message : "success"});
 }
