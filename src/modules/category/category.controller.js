751bafd33b60412921f5dcaca9cbbe31d0f131b4
mohammadsalamin <msalameen.123456@gmail.com>
2025-03-14 00:02:12 +0200

    update and delete category

--- src/modules/category/category.controller.js
+++ src/modules/category/category.controller.js
@@ -20,3 +20,33 @@ export const getActiveCategories = async (req, res) =>{
     const categories = await CategoryModel.find({status : 'active'});
     return res.status(200).json({message : "success", categories});
 }
+
+export const updateCategory = async (req, res) =>{
+    const {id} = req.params;
+    const {name} = req.body;
+    const userId = req.id;
+    
+    const category = await CategoryModel.findById(id);
+    if(!category){
+        return res.status(404).json({message : 'category not found'});
+    }
+
+    category.name = name;
+    category.slug = slugify(name);
+    category.status = req.body.status;
+    category.updatedBy = userId;
+
+    await category.save(); //save changes
+
+    return res.status(200).json({message : 'success'});
+}
+
+export const removeCategory = async (req, res) =>{
+    const {id} = req.params;
+
+    const category = await CategoryModel.findByIdAndDelete(id);
+    if(!category){
+        return res.status(404).json({message : 'category not found'});
+    }
+    return res.status(200).json({message : 'success'});
+}
--- src/modules/category/category.router.js
+++ src/modules/category/category.router.js
@@ -6,5 +6,7 @@ const router = Router();
 router.post('/create',auth(['admin']), categoryController.create);
 router.get('/', auth(['admin', 'user']), categoryController.getCategories);
 router.get('/active', categoryController.getActiveCategories);
+router.put('/:id', auth(['admin']), categoryController.updateCategory);
+router.delete('/:id',auth(['admin']), categoryController.removeCategory);
 
 export default router;
