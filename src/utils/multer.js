159182d3b5b4d12e7c6b27ed77e9c581bd5cb272
mohammadsalamin <msalameen.123456@gmail.com>
2025-03-14 01:11:01 +0200

    file upload (multer.js and cloudinary.js)

---
+++ src/utils/multer.js
@@ -0,0 +1,24 @@
+import multer from 'multer';
+
+export const fileValidation = {
+    image : ['image/png', 'image/jpg', 'image/jpeg', 'image/webp'],
+    pdf : ['application/pdf'],
+    excel : ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']
+}
+
+const fileUpload = (customValidation = []) =>{
+    const storage = multer.diskStorage({});
+
+    function fileFilter(req, file, cb){
+        if(customValidation.includes(file.memeType)){
+            cb(null, true);
+        }else{
+            cb(new Error('invalid file type'), false);
+        }
+    }
+
+    const upload = multer({fileFilter, storage});
+    return upload;
+}
+
+export default fileUpload;
